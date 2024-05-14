d1518b7235949e06c32118152db874dd2e2bd9f1
Rahman Hafidzin <rhafidzin@gmail.com>
2024-05-14 08:58:24 +0700

    push responsive tailwind

---
+++ gen-20-tailwind-responsive/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        color: {
+          primary: '#FAF7F2ff'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
